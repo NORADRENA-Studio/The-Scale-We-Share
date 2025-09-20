db004bd65358fc6d798e4c28875bb27f03d92585
Simion Petrica <petricasimion69@gmail.com>
2025-09-20 15:19:00 +0300

    Debug: Test PR pipeline

---
+++ GoodTest.cpp
@@ -0,0 +1,24 @@
+﻿#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+// ✅ Clasa are prefix "A"
+class AMyGoodActor : public AActor
+{
+public:
+    // ✅ Variabilă membră în PascalCase
+    float HealthPoints;
+
+    // ✅ Boolean cu prefix b
+    bool bIsAlive;
+
+    // ✅ Funcția în PascalCase
+    void TakeDamage(float damageAmount)
+    {
+        // ✅ Variabile locale în camelCase
+        float newHealth = HealthPoints - damageAmount;
+        bool bShouldDie = newHealth <= 0.0f;
+
+        HealthPoints = newHealth;
+        bIsAlive = !bShouldDie;
+    }
+};
